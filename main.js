7c9c0f9a5d655f45b6f14e03d519bfd04ac5459b
TooMuchTM87 <armanicheong808@gmail.com>
2024-02-22 00:11:18 -1000

    title done

---
+++ main.js
@@ -0,0 +1,9 @@
+let title = document.querySelector(".title");
+let titleText = document.createElement("div");
+title.appendChild(titleText);
+titleText.textContent = "Rock Paper Scissors";
+titleText.style.fontSize = "xx-large";
+titleText.style.display = "flex";
+titleText.style.justifyContent = "center";
+
+
